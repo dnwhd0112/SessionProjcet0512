ef1def8d3ca25912192164d5c48a210c18afe311
dnwhd0112 <dnwhd0112@naver.com>
2022-05-12 14:30:17 +0900

    Initial Commit

---
+++ TableViewSessionProjectUITests/TableViewSessionProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TableViewSessionProjectUITestsLaunchTests.swift
+//  TableViewSessionProjectUITests
+//
+//  Created by 곽우종 on 2022/05/12.
+//
+
+import XCTest
+
+class TableViewSessionProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
